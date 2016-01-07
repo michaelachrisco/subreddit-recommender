63f954e5dbfaa27b954475cc53c1514910c5c46c
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-01-07 08:53:47 -0800

    Answer "Yes" automatically to apt-get prompts about whether to install packages

--- script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
+++ script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install synaptic git-daemon-sysvinit gitg
-sudo apt-get --install-suggests install nodejs
-sudo apt-get --install-suggests install nodejs-legacy
-sudo apt-get --install-suggests install npm ruby
-sudo apt-get --install-suggests install rails git git-hub libgsl0-dev libgsl0-dbg libpq-dev ruby-gsl node-lodash agrep sqlite3
+sudo apt-get --yes install synaptic git-daemon-sysvinit gitg
+sudo apt-get --yes --install-suggests install nodejs
+sudo apt-get --yes --install-suggests install nodejs-legacy
+sudo apt-get --yes --install-suggests install npm ruby
+sudo apt-get --yes --install-suggests install rails git git-hub libgsl0-dev libgsl0-dbg libpq-dev ruby-gsl node-lodash agrep sqlite3
 
 sudo npm install bower -g
 
