51e7b4b470721c5b96c8151792a209a2bd31fa12
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-01-06 19:45:09 -0800

    Move Ubuntu 15.10 scripts under "script" subdirectory

--- ubuntu-15-10-setup-and-run-subreddit-recommender.bash
+++ ubuntu-15-10-setup-and-run-subreddit-recommender.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
-#./ubuntu/15.10/setup-git-hub.bash
-#./ubuntu/15.10/setup-postgres.bash
+./script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
+#./script/ubuntu/15.10/setup-git-hub.bash
+#./script/ubuntu/15.10/setup-postgres.bash
 
-#./ubuntu/15.10/download-subreddit-recommender.bash
-./ubuntu/15.10/build-and-run-subreddit-recommender.bash
+./script/ubuntu/15.10/download-subreddit-recommender.bash
+./script/ubuntu/15.10/build-and-run-subreddit-recommender.bash
