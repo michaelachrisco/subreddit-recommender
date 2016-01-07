461917cf6cda0f749315ae1a230de5bcec7763ae
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-01-07 06:58:06 -0800

    Uncomment setup-git-hub and setup-postgres scripts in main Ubuntu script

--- ubuntu-15-10-setup-and-run-subreddit-recommender.bash
+++ ubuntu-15-10-setup-and-run-subreddit-recommender.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
-#./script/ubuntu/15.10/setup-git-hub.bash
-#./script/ubuntu/15.10/setup-postgres.bash
+./script/ubuntu/15.10/setup-git-hub.bash
+./script/ubuntu/15.10/setup-postgres.bash
 
 ./script/ubuntu/15.10/download-subreddit-recommender.bash
 ./script/ubuntu/15.10/build-and-run-subreddit-recommender.bash
