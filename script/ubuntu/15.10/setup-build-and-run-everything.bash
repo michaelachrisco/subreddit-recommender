064b442b106cd2bdfb87dce03afe26c3e27c9bf0
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-01-07 10:18:24 -0800

    Set script execute permissions automatically, as much as possible

--- script/ubuntu/15.10/setup-build-and-run-everything.bash
+++ script/ubuntu/15.10/setup-build-and-run-everything.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+chmod a+x ./setup-subreddit-recommender-prereqs.bash
+chmod a+x ./setup-git-hub.bash
+chmod a+x ./setup-postgres.bash
+
+chmod a+x ./download-subreddit-recommender.bash
+chmod a+x ./config.bash
+chmod a+x ./build-and-run-subreddit-recommender.bash
+
 ./setup-subreddit-recommender-prereqs.bash
 ./setup-git-hub.bash
 ./setup-postgres.bash
--- ubuntu-15-10-setup-and-run-everything.bash
+++ ubuntu-15-10-setup-and-run-everything.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ./script/ubuntu/15.10/
+chmod a+x ./setup-build-and-run-everything.bash
 ./setup-build-and-run-everything.bash
