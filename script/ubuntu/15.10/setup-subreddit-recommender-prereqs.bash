ec1970b9388545eaafb4622bd27c5551e35ba4e5
stephengtuggy <sgt@StephenGTuggy.com>
2016-01-31 17:39:34 -0800

    More on Ubuntu 15.10 scripts

--- script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
+++ script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
@@ -4,7 +4,7 @@ sudo apt-get --yes install synaptic git-daemon-sysvinit gitg
 sudo apt-get --yes --install-suggests install nodejs
 sudo apt-get --yes --install-suggests install nodejs-legacy
 sudo apt-get --yes --install-suggests install npm ruby
-sudo apt-get --yes --install-suggests install rails git git-hub libgsl0-dev libgsl0-dbg libpq-dev ruby-gsl node-lodash agrep sqlite3
+sudo apt-get --yes --install-suggests install rails git git-hub libgsl0-dev libgsl0-dbg libpq-dev ruby-gsl node-lodash agrep sqlite3 libsqlite3-dev
 
 sudo npm install bower -g
 
