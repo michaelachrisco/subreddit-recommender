d1faea7a915fc516841d0444ee386b3232867e49
stephengtuggy <sgt@StephenGTuggy.com>
2016-02-04 10:16:12 -0800

    Get rid of pesky krb5-config pseudo-dependency that requires manual configuration

--- script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
+++ script/ubuntu/15.10/setup-subreddit-recommender-prereqs.bash
@@ -4,6 +4,7 @@ sudo apt-get --yes                    install synaptic git-daemon-sysvinit gitg
 sudo apt-get --yes --install-suggests install nodejs
 sudo apt-get --yes --install-suggests install nodejs-legacy
 sudo apt-get --yes --install-suggests install npm ruby
-sudo apt-get --yes --install-suggests install rails git libgsl0-dev libgsl0-dbg libpq-dev ruby-gsl node-lodash agrep sqlite3 libsqlite3-dev #git-hub
+sudo apt-get --yes --install-suggests install rails git libgsl0-dev libgsl0-dbg ruby-gsl node-lodash agrep sqlite3 libsqlite3-dev #git-hub
+sudo apt-get --yes                    install libpq-dev
 
 sudo npm install bower -g
