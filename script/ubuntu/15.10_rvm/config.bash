502c5cfab62208a766cc56fae92dfbe81b57cf66
stephengtuggy <sgt@StephenGTuggy.com>
2016-03-02 17:44:28 -0800

    Put all the rvm scripts in one file; hopefully it will work better this way

--- script/ubuntu/15.10_rvm/config.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd ~/repos/git/michaelachrisco/subreddit-recommender/config/
-cp database.yml.ig database.yml
-cp secrets.yml.ig secrets.yml
-popd
-
-export RAILS_ENV=development
--- script/ubuntu/15.10_rvm/download-subreddit-recommender.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/repos/git/michaelachrisco && cd $_
-git clone https://github.com/michaelachrisco/subreddit-recommender.git
-cd subreddit-recommender
-ls -l
-git status
